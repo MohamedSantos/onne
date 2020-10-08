30810735b0a730567a2f8f6cd350fc247b644623
Martins Pereira <martins.silva@etec.sp.gov.br>
2020-10-08 13:10:19 -0300

    atualização do accordion

--- js/accordionMenu.js
+++ js/accordionMenu.js
@@ -2,14 +2,16 @@ export default function accordionMenu() {
   let accordion = document.querySelector('.accordion')
   let menu = document.querySelector('.menu-list')
 
-  accordion.addEventListener('click', () => {
-    console.log(menu.classList.contains('show'))
-    if (menu.classList.contains('show')) {
-      menu.classList.remove('show')
-      accordion.classList.remove('active')
-    } else {
-      menu.classList.add('show');
-      accordion.classList.add('active');
-    }
-  })
+  if (menu && accordion) {
+    accordion.addEventListener('click', () => {
+      if (menu.classList.contains('show')) {
+        menu.classList.remove('show')
+        accordion.classList.remove('active')
+      } else {
+        menu.classList.add('show');
+        accordion.classList.add('active');
+      }
+    })
+  }
+
 }
