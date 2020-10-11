e045c24b02c7be8d2712a78c382a3dae51cfae76
Martins Pereira <martins.silva@etec.sp.gov.br>
2020-10-11 16:13:18 -0300

    mudança no binary

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 import accordionMenu from './accordionMenu.js';
 import menuDropdown from './menuDropdown.js';
+import changeTextBinary from './changeTextBinary.js';
+import listUnilevel from './listUnilevel.js';
+
 
 accordionMenu();
 menuDropdown();
