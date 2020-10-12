012f9a594c2ba98eafa4bfece159c19b111d2f1d
Thiago Alves da Silva <thiago.silva@claravista.com.br>
2020-10-12 11:46:33 -0300

    Tooltip ajustada;

--- js/tooltip.js
+++ js/tooltip.js
@@ -3,21 +3,32 @@ export default () => {
     const container = document.createElement('div');
     container.innerText = dataset.text;
     container.classList.add('tooltip');
+    if (window.innerWidth <= 800) {
+      const link = document.createElement('a');
+      link.innerText = 'See more'
+      link.href = '#';
+      container.appendChild(link);
+    }
     document.body.appendChild(container);
     return container;
   }
 
-  const position = (target, pageY, pageX) => {
-    if (window.innerWidth > 600) {
+  const position = (target, pageY, pageX, tooltip) => {
+    if (window.innerWidth > 800) {
       return {
         top: `${pageY}px`,
         left: `${pageX + 20}px`
       };
     }
-    return {
-      top: `${target.parentNode.offsetTop}px`,
-      left: `${target.parentNode.offsetLeft + target.clientWidth}px`
+
+    let left = target.parentNode.offsetLeft + target.clientWidth;
+    if (tooltip.clientWidth + left >= window.innerWidth) {
+      left -= (tooltip.clientWidth + target.clientWidth + 15);
     }
+    return {
+      top: `${target.parentNode.offsetTop - target.clientHeight / 4}px`,
+      left: `${left + 5}px`
+    };
   }
 
   const handleMouseLeave = {
@@ -30,17 +41,18 @@ export default () => {
 
   const handleMouseMove = {
     handleEvent({ target, pageX, pageY }) {
-      const { top, left } = position(target, pageY, pageX);
+      const { top, left } = position(target, pageY, pageX, this.tooltip);
       this.tooltip.style.top = top;
       this.tooltip.style.left = left;
     }
   }
 
   const handleMouseOver = ({ target, pageY, pageX }) => {
-    const { top, left } = position(target, pageY, pageX);
     const tooltip = createBox(target);
-    tooltip.style.top = top;
+    const { top, left } = position(target, pageY, pageX, tooltip);
+
     tooltip.style.left = left;
+    tooltip.style.top = top;
 
     handleMouseLeave.target = target;
     handleMouseLeave.tooltip = tooltip;
