7ee1a0860593613b78437f83485d79f60808da14
Martins Pereira <martins.silva@etec.sp.gov.br>
2020-10-12 12:16:50 -0300

    menu responsivo

--- js/accordionMenu.js
+++ js/accordionMenu.js
@@ -1,6 +1,8 @@
 export default function accordionMenu() {
   const accordion = document.querySelector('.accordion');
+  const accordion2 = document.querySelectorAll('.accordion2');
   const menu = document.querySelector('.wrapper');
+  const menu2 =document.querySelector('.header-menu')
 
   if (menu && accordion) {
     accordion.addEventListener('click', () => {
@@ -8,5 +10,15 @@ export default function accordionMenu() {
       menu.classList[hasClass ? 'remove' : 'add']('show');
       accordion.classList[hasClass ? 'remove' : 'add']('active');
     });
+  }
+  if(accordion2){
+    accordion2.forEach(item => {
+      item.addEventListener('click', () => {
+        const hasClass = menu2.classList.contains('show');
+        menu2.classList[hasClass ? 'remove' : 'add']('show');
+        item.classList[hasClass ? 'remove' : 'add']('active');
+      });
+    })
+    
   }
 }
