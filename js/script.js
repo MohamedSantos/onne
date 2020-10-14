6ba88dfad041b9d49f65c68d3274fd5fb7ff4dc7
Thiago Alves da Silva <thiago.silva@claravista.com.br>
2020-10-14 14:32:08 -0300

    reversão de merge

--- js/script.js
+++ js/script.js
@@ -6,13 +6,14 @@ accordionMenu();
 menuDropdown();
 tooltip();
 
-function onlynumber(event){
+function onlynumber(event) {
     var theEvent = event || window.event;
     var key = theEvent.keyCode || theEvent.which;
     key = String.fromCharCode(key);
     var regex = /^[0-9.,]+$/;
-    if(!regex.test(key)){
+    if (!regex.test(key)) {
         theEvent.returnValue = false;
-        if(theEvent.preventDefault) theEvent.preventDefault();
+        if (theEvent.preventDefault) theEvent.preventDefault();
     }
+
 }
